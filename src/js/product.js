a2ceda4533c32c3e98bbbe048c9e74ae4f19f139
herbertmoroni <herbertmoroni@hotmail.com>
2024-09-25 15:35:14 -0600

    issue fixed

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,29 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart");
+
+  if (!cart) {
+    cart = [];
+  }
+
+  if (!Array.isArray(cart)) {
+    cart = [cart];
+  }
+
+  cart.push(product);
+ 
+  setLocalStorage("so-cart", cart);
+
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
+
+  window.location.href = "/cart/index.html";
 }
 
 // add listener to Add to Cart button
