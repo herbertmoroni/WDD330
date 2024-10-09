55b2c412ecbc3748c29b06602b155d5f7586bba8
herbertmoroni <herbertmoroni@hotmail.com>
2024-10-09 09:37:36 -0600

    fix error with query selector

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,18 +1,16 @@
 import { getData } from "./productData.mjs";
 export default async function productList() {
-    const productList = document.querySelector("product-list");
+    const productList = document.querySelector(".product-list");
     const products = await getData();
-    console.log(products);
     products.forEach(element => {
         const p = productCard(element);
-        console.log(p);
         productList.append(p);
     });
 }
 function productCard(product) {
     const template = document.getElementById("product-card-template");
-    const clone = template.cloneNode(true);
-    const name = clone.querySelector("card__name");
+    const clone = template.content.cloneNode(true);
+    const name = clone.querySelector(".card__name");
     name.textContent = product.NameWithoutBrand;
     return clone;
 }
