d0b6d58bc344842cbc01293641970ec09104aaef
herbertmoroni <herbertmoroni@hotmail.com>
2024-10-15 20:09:21 -0600

    week 5 first version

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,26 @@
 import productList from "./productList.mjs";
 import fetchAlerts from "./alert.mjs";
+import { loadHeaderFooter }  from "./utils.mjs";
 
 productList();
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchAlerts();
 }); //added fetchAlerts here
+
+// const headerTemplateFn = loadTemplate("/partials/header.html");
+// const footerTemplateFn = loadTemplate("/partials/footer.html");
+
+// const headerElement = document.getElementById("main-header");
+// const footerElement = document.getElementById("main-footer");
+
+
+// headerTemplateFn().then((template) => {
+//     headerElement.innerHTML = template;
+// }); 
+
+// footerTemplateFn().then((template) => {    
+//     footerElement.innerHTML = template;
+// }); 
+
+loadHeaderFooter();
