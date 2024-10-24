41b84648f85344aa9e67192cdd47dc72fb83751c
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-24 01:12:08 -0500

    discount indicator

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -1,13 +1,14 @@
 import { getLocalStorage } from "./utils.mjs";
 
 export function renderCartContents() {
-    const cartItems = getLocalStorage("so-cart");
-    const htmlItems = cartItems ? cartItems.map((item) => cartItemTemplate(item)) : [];
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const cartItems = getLocalStorage("so-cart");
+  const htmlItems = cartItems ? cartItems.map((item) => cartItemTemplate(item)) : [];
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
-  
+
 function cartItemTemplate(item) {
-    const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider">
+    <button class="cart-card__remove" data-id="${item.id}">❌</button>
     <a href="#" class="cart-card__image">
       <img
         src="${item.Image}"
@@ -21,6 +22,15 @@ function cartItemTemplate(item) {
     <p class="cart-card__quantity">qty: 1</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
-  
-    return newItem;
+
+  return newItem;
+}
+
+//working on this 
+document.querySelector(".product-list").addEventListener("click", function (event) {
+  if (event.target.classList.contains("cart-card__remove")) {
+    const itemId = event.target.getAttribute("data-it");
+    removeFromCart(itemId);
+    renderCartContents();
   }
+}) 
