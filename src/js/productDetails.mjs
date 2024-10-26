f3a5c7bf76b9c1e83ecd4c1511aac51c281b51f2
herbertmoroni <herbertmoroni@hotmail.com>
2024-10-26 17:21:38 -0600

    fix image product detail page

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -23,7 +23,7 @@ export function addToCart() {
 export function renderProductDetails() {
     document.querySelector("#productName").innerText = product.Brand.Name;
     document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
-    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").src = product.Images.PrimaryLarge;
     document.querySelector("#productImage").alt = product.Name;
     document.querySelector("#productColorName").innerText = product.Colors[0].ColorName;
     document.querySelector("#productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
