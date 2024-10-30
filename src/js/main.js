f3c543db6072a998d68e5562bb2d987d6734bafa
karlarum <131808949+karlarum@users.noreply.github.com>
2024-10-30 00:37:39 -0500

    created newletter sign up form

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
 import productList from "./productList.mjs";
 import fetchAlerts from "./alert.mjs";
-import { loadHeaderFooter  }  from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+import { createNewsletterForm } from "./newsletterForm.mjs";
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     loadHeaderFooter();
 
+    createNewsletterForm();
+
     productList();
-    
+
     fetchAlerts();
-}); 
+});
 
 
